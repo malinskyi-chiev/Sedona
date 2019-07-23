5df15666dd643e078d4febc57cded074572fccc0
Fitch-Chievel <fitchchievelgit@gmail.com>
2019-07-23 09:53:57 +0300

    add favicon

--- source/js/index-silent.js
+++
@@ -1,8 +0,0 @@
-// var washItems = function(itemsLeft) {
-//     debugger;
-//     while (itemsLeft-- > 0) {
-//         console.log('В раковине осталось ' + itemsLeft + ' предметов');
-//     }
-// }
-
-// washItems(10);
